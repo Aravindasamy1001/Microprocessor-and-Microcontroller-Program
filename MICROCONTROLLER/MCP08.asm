f962415c51af051cc0280348a02058bcf32eb5ca
Aravindasamy v <aravindasamy.v.1001@gmail.com>
2026-04-13 18:35:18 +0530

    Create MCP08.asm

---
+++ MICROCONTROLLER/MCP08.asm
@@ -0,0 +1,16 @@
+ORG 0000H
+	MOV A,#0FEH
+	
+	LOOP: MOV P1,A
+	      ACALL DELAY
+		  
+		  RL A
+		  SJMP LOOP
+		  
+	DELAY: MOV R1,#255
+	       D1:MOV R2,#255
+		   D2:DJNZ R2,D2
+		   DJNZ R1,D1
+		   RET
+		   
+END
