b408509526bbe2838b52f88c9c838956244e2bdb
Aravindasamy v <aravindasamy.v.1001@gmail.com>
2026-04-16 09:32:03 +0530

    Add files via upload

---
+++ EMBEDDED_C/emc03.c
@@ -0,0 +1,23 @@
+#include<reg51.h>
+
+void delay(){
+	unsigned int i,j;
+for(i=0;i<=255;i++){
+			for(j=0;j<=255;j++){
+			}
+		}
+	}
+	void main(){
+  
+		while(1){
+		
+		P2 = 0x00;
+		
+	  delay();	
+  
+    P2 = 0xFF;
+	  delay();
+		
+		}
+	
+}	
